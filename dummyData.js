a976ed2fd4b9e364685dbb46e90403a9faad1820
Jessica Madamba <jessica.madamba@gmail.com>
2019-02-12 17:16:46 -0800

    Implement Recently Viewed section with dummy data

---
+++ dummyData.js
@@ -0,0 +1,52 @@
+let dummyData = [
+  {
+      "id": 1,
+      "name": "Tiger Wool Blend Sweater",
+      "brand": "Gucci",
+      "price": "$1,500.00",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/1a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/1b.jpeg"
+  },
+  {
+      "id": 2,
+      "name": "Faux Leather Leggings",
+      "brand": "Spanx",
+      "price": "$98.00",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/2a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/2b.jpeg"
+  },
+  {
+      "id": 3,
+      "name": "Gravitissima Thigh High Boot",
+      "brand": "Christian Louboutin",
+      "price": "$1,895.00",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/3a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/3b.jpeg"
+  },
+  {
+      "id": 4,
+      "name": "Diamond Lux Pavé Station Rope Bracelet",
+      "brand": "Lagos",
+      "price": "$20,000.00",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/4a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/4b.jpeg"
+  },
+  {
+      "id": 5,
+      "name": "Luxe Faux Fur Coat",
+      "brand": "Topshop",
+      "price": "$180.00",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/5a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/5b.jpeg"
+  },
+  {
+      "id": 6,
+      "name": "Belmont Mock Neck Sweater",
+      "brand": "Madewell",
+      "price": "$69.99",
+      "front_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/6a.jpeg",
+      "back_pic": "https://s3-us-west-1.amazonaws.com/recently-viewed-items/6b.jpeg"
+  }
+]
+
+export default dummyData;
